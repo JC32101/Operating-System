6fc154913dca970bd08a7ca44b56f21350b3d74f
GreedySkunk <lk78448n@pace.edu>
2021-10-23 23:15:40 -0400

    moving task to proper directory, saving old task 3 version, updated serving as official version

--- project1/task3/Block.java
+++ project1/task3/Block.java
@@ -1,44 +1,20 @@
 package task3;
 
-import java.util.Comparator;
+public class Block {
 
-class Block {
-    int offset;
-    int size;
-    Block next;
+        int offset;
+        int allosize;
+        Block next;
 
-
-    Block(int offset, int size){
-        this.offset = offset;
-        this.size = size;
-    }
+        Block(int offset, int size){
+            this.offset = offset;
+            this.allosize = size;
+        }
 
         Block(int offset, int size, Block next){
-        this.offset = offset;
-        this.size = size;
-        this.next = next;
-    }
-
-    public String toString() // highly recommended
-    {
-        return "Offset: " + offset + "\nSize: " + size;
-    }
-
-    public boolean is_adjacent(Block other) {
-        if((offset == other.offset) && (size == other.size)){
-            return true;
+            this.offset = offset;
+            this.allosize = size;
+            this.next = next;
         }
-        else {
-            return false;
-        }
-    }
-}
-
-// Sort-by in Java: (needs a class)
-// You may need it to sort your list or you can maintain a sorted list upon insertion
-class ByOffset implements Comparator<Block> {
-    @Override
-    public int compare(Block lhs, Block rhs) {
-        return Integer.compare(lhs.offset, rhs.offset);
-    }
+          
 }
